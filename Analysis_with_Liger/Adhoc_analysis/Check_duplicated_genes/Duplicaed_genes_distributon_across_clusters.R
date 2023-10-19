28f58e87d2e5767dc03c5b217be4ee5194009938
ytamal2 <ytamal2@mpipz.mpg.de>
2023-10-19 18:18:40 +0200

    Updated files

--- Analysis_with_Liger/Adhoc_analysis/Check_duplicated_genes/Duplicaed_genes_distributon_across_clusters.R
+++ Analysis_with_Liger/Adhoc_analysis/Check_duplicated_genes/Duplicaed_genes_distributon_across_clusters.R
@@ -14,4 +14,4 @@ input_file = as.data.frame(read_excel(path = "List_duplicates_Cardamine.xlsx", c
 input_file$gene_ID = str_replace(string = sub(pattern = "^(.*_.*)_.*", replacement = "\\1", x = input_file$Chr_genes), pattern = "_", replacement = "-")
 
 # Call the function with desired parameters
-features_weight_across_clusters(seuratObject = your_seurat_object, features_list = input_file, search_max = 3, include_cell_count = TRUE, store_dir = "your_path")
+features_weight_across_clusters(seuratObject = integrated.data, features_list = a, search_max = 2, include_cell_count = TRUE, store_dir = getwd())
